0655d2c291ad9c0d2e0e86fc1510e4c65b938b37
DariaTiessova <bermuds@inbox.ru>
2020-09-14 21:33:38 +0600

    Scene loader

--- Assets/Scripts/ZoneActivisation.cs
+++ Assets/Scripts/ZoneActivisation.cs
@@ -1,32 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ZoneActivisation : MonoBehaviour
 {
-    public GameObject sphere;
-    public GameObject ramp2;
-    public GameObject ramp1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
-        {
-            
-            ramp2.transform.rotation = Quaternion.identity;
-            ramp2.SetActive(true);
-            ramp1.SetActive(false);
-        }
+        if(other.tag=="Player")
+        SceneManager.LoadScene("Scene_2");
+          
     }
 }
